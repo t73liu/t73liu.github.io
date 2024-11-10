ea7401cee05a00d4823ca58e6e3e5cbc5a476e01
Tao <t73liu@gmail.com>
2024-11-09 22:48:43 -0500

    feat: Remove cloudflare analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import mdx from "@astrojs/mdx";
-import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
@@ -11,12 +10,6 @@ export default defineConfig({
     mdx(),
     sitemap(),
     robotsTxt(),
-    // TODO: Confirm this is required.
-    // https://docs.astro.build/en/guides/integrations-guide/partytown/#configforward
-    // config: {
-    //   forward: ["dataLayer.push"],
-    // },
-    partytown(),
     tailwind(),
   ],
   markdown: {
